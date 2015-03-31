f0bdb8fb34d95639366eb37f78cf3b8dca093b67
Rodrigo Agerri <ragerri@apache.org>
2015-03-31 22:05:05 +0200

    removing apl

--- src/main/java/es/ehu/si/ixa/ixa/pipe/tok/WhiteSpaceTokenizer.java
+++ src/main/java/es/ehu/si/ixa/ixa/pipe/tok/WhiteSpaceTokenizer.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Rodrigo Agerri
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
-
 package es.ehu.si.ixa.ixa.pipe.tok;
 
 import java.io.BufferedReader;
@@ -31,7 +15,6 @@ import es.ehu.si.ixa.ixa.pipe.tok.jflex.WhiteSpaceLexer;
  *  The tokenizer detects whitespaces to separate tokens. It can also detect 
  *  and mark newlines and paragraphs using the CLI option --paragraphs.
  *  
- * @author ragerri
  * @version 2014-01-30
  * 
  */
